749deb57d533aa2137404542e75145d55504d8be
Sergei Chugainov <chsev@yandex.ru>
2018-05-18 11:33:18 +0500

    Змейка забегала

--- vcworm/Form1.h
+++ vcworm/Form1.h
@@ -55,6 +55,7 @@ namespace vcworm {
 		/// <summary>
 		SNAKE_MODEL::VCView * m_view;
 	private: System::Windows::Forms::Timer^  timer1;
+	private: System::Windows::Forms::PictureBox^  pictureBox1;
 
 
 	private: System::ComponentModel::IContainer^  components;
@@ -76,13 +77,16 @@ namespace vcworm {
 			this->buttonStart = (gcnew System::Windows::Forms::Button());
 			this->labelScore = (gcnew System::Windows::Forms::Label());
 			this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
 			this->panel1->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
 			this->label1->Location = System::Drawing::Point(27, 12);
+			this->label1->Margin = System::Windows::Forms::Padding(3, 10, 3, 0);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(44, 13);
 			this->label1->TabIndex = 0;
@@ -90,16 +94,16 @@ namespace vcworm {
 			// 
 			// panel1
 			// 
-			this->panel1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
 			this->panel1->AutoSize = true;
 			this->panel1->AutoSizeMode = System::Windows::Forms::AutoSizeMode::GrowAndShrink;
 			this->panel1->Controls->Add(this->buttonQuit);
 			this->panel1->Controls->Add(this->buttonStart);
 			this->panel1->Controls->Add(this->labelScore);
 			this->panel1->Controls->Add(this->label1);
-			this->panel1->Location = System::Drawing::Point(310, 12);
+			this->panel1->Dock = System::Windows::Forms::DockStyle::Right;
+			this->panel1->Location = System::Drawing::Point(322, 0);
 			this->panel1->Name = L"panel1";
-			this->panel1->Size = System::Drawing::Size(94, 124);
+			this->panel1->Size = System::Drawing::Size(94, 317);
 			this->panel1->TabIndex = 1;
 			// 
 			// buttonQuit
@@ -138,20 +142,34 @@ namespace vcworm {
 			// 
 			this->timer1->Tick += gcnew System::EventHandler(this, &Form1::timer1_Tick);
 			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->BackColor = System::Drawing::SystemColors::Control;
+			this->pictureBox1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->pictureBox1->Location = System::Drawing::Point(0, 0);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(322, 317);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::AutoSize;
+			this->pictureBox1->TabIndex = 2;
+			this->pictureBox1->TabStop = false;
+			this->pictureBox1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &Form1::pictureBox1_Paint);
+			this->pictureBox1->SizeChanged += gcnew System::EventHandler(this, &Form1::pictureBox1_SizeChanged);
+			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->CancelButton = this->buttonQuit;
 			this->ClientSize = System::Drawing::Size(416, 317);
+			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->panel1);
 			this->KeyPreview = true;
 			this->Name = L"Form1";
 			this->Text = L"Form1";
-			this->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &Form1::Form1_Paint);
 			this->KeyPress += gcnew System::Windows::Forms::KeyPressEventHandler(this, &Form1::Form1_KeyPress);
 			this->panel1->ResumeLayout(false);
 			this->panel1->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -169,36 +187,10 @@ namespace vcworm {
 				timer1->Enabled = false;
 				m_view->nextStep();
 				labelScore->Text = "" + m_view->getSnake()->size();
-				Invalidate();
+				pictureBox1->Invalidate();
 				if (!m_view->isPause())
 					timer1->Enabled = true;
 				}
-	private: System::Void Form1_Paint(System::Object^  sender, System::Windows::Forms::PaintEventArgs^  e) {
-				SNAKE_MODEL::GraphicSnake * snake = m_view->getSnake();
-				SNAKE_MODEL::RabbitFactory * rabbits = m_view->getRabbitFactory();
-				Graphics^ g = e->Graphics;
-				Pen^ blackPen = gcnew Pen( Color::Black,3.0f );
-//				g->DrawLine( System::Drawing::Pens::Red,0,0,10,10);
-//				g->DrawLine( System::Drawing::Pens::Red, pictureBox1->Left, pictureBox1->Top,
-//				pictureBox1->Right, pictureBox1->Bottom );
-				for (std::vector<SNAKE_MODEL::GraphicPoint>::iterator iter = snake->begin(); iter != snake->end(); iter++) {
-					Rectangle rect = Rectangle(iter->getX()*10, iter->getY() * 10,10,10);
-					g->DrawRectangle( blackPen, rect );
-				}
-
-				for (std::vector<SNAKE_MODEL::Rabbit>::iterator iter = rabbits->begin(); iter != rabbits->end(); iter++) {
-					Rectangle rect = Rectangle(iter->getX()*10, iter->getY() * 10,10,10);
-					g->DrawRectangle( blackPen, rect );
-				}
-
- //Pen^ blackPen = gcnew Pen( Color::Black,3.0f );
-
-      // Create rectangle.
-//      Rectangle rect = Rectangle(0,0,200,200);
-
-      // Draw rectangle to screen.
-      //e->Graphics->DrawRectangle( blackPen, rect );
-			 }
 private: System::Void Form1_KeyPress(System::Object^  sender, System::Windows::Forms::KeyPressEventArgs^  e) {
 			 switch (e->KeyChar) {
 //				 case Keys::Left:
@@ -223,6 +215,91 @@ private: System::Void Form1_KeyPress(System::Object^  sender, System::Windows::F
 					break;
 			 }
 		 }
+private: System::Void pictureBox1_Paint(System::Object^  sender, System::Windows::Forms::PaintEventArgs^  e) {
+			SNAKE_MODEL::GraphicSnake * snake = m_view->getSnake();
+			SNAKE_MODEL::RabbitFactory * rabbits = m_view->getRabbitFactory();
+			Graphics^ g = e->Graphics;
+
+			const int bh = m_view->getHieghtField();
+			const int bw = m_view->getWidthField();
+			int h = pictureBox1->Height/bh;
+			int w = pictureBox1->Width/bw;
+
+			if (h < w) 
+				w = h;
+			else 
+				h = w;
+
+			SolidBrush^ blueBrush = gcnew SolidBrush( Color::Blue );
+			SolidBrush^ greenBrush = gcnew SolidBrush( Color::Green );
+			SolidBrush^ redBrush = gcnew SolidBrush( Color::Red );
+			SolidBrush^ yellowBrush = gcnew SolidBrush( Color::Yellow );
+
+			g->Clear(System::Drawing::SystemColors::Control);
+			g->FillRectangle(gcnew SolidBrush(System::Drawing::SystemColors::ControlDark),
+				0, 0, w * bw, h * bh);
+
+			Pen^ blackPen = gcnew Pen( Color::Black,3.0f );
+//				g->DrawLine( System::Drawing::Pens::Red,0,0,10,10);
+			for (std::vector<SNAKE_MODEL::GraphicPoint>::iterator iter = snake->begin(); iter != snake->end(); iter++) {
+	            SolidBrush^ color = (iter - snake->begin()) % 5 == 3 ? yellowBrush: redBrush;
+		        drawSnake(g, &*iter, w, h, color);
+			}
+
+			for (std::vector<SNAKE_MODEL::Rabbit>::iterator iter = rabbits->begin(); iter != rabbits->end(); iter++) {
+				Rectangle rect = Rectangle(iter->getX()*w + 1, iter->getY()*h + 1, w - 2, h - 2);
+				g->FillRectangle(greenBrush, rect);
+			}
+
+		 }
+private: System::Void pictureBox1_SizeChanged(System::Object^  sender, System::EventArgs^  e) {
+			pictureBox1->Invalidate();
+		 }
+
+private: void drawSnake(Graphics^ g, SNAKE_MODEL::GraphicPoint *point, int w, int h, SolidBrush^ brush) {
+    Rectangle body1(point->getX() * w, point->getY() * h, w, h);
+
+    int ddx = w / 5, ddx2 = ddx + ddx;
+    int ddy = h / 5, ddy2 = ddy + ddy;
+
+    switch (point->getType()) {
+    case SNAKE_MODEL::GraphicPoint::HEAD:
+        g->FillRectangle(brush, body1);
+        return;
+
+    case SNAKE_MODEL::GraphicPoint::HORISONTAL:
+		body1.Y += ddy * point->getPosition();
+		body1.Height = w - ddy2;
+        g->FillRectangle(brush, body1);
+        return;
+
+    case SNAKE_MODEL::GraphicPoint::VERTICAL:
+		body1.X += ddx * point->getPosition();
+		body1.Width = h - ddx2;
+        g->FillRectangle(brush, body1);
+        break;
+
+    case SNAKE_MODEL::GraphicPoint::CORNER:
+		body1.Width = h - ddx2;
+		body1.Height = w - ddy2;
+        if (point->getPosition()) {
+			Rectangle body2 = body1;
+			body2.X += point->getToLeft() ? 0 : ddx2;
+			body2.Y += ddy;
+			g->FillRectangle(brush, body2);
+
+			body1.X += ddx; 
+			body1.Y += point->getToUp() ? 0:ddy2;
+		
+			g->FillRectangle(brush, body1);
+		} else {
+			body1.X += point->getToLeft() ? 0 : ddx2;
+			body1.Y += point->getToUp() ? 0 : ddy2;
+			g->FillRectangle(brush, body1);
+		}
+    }
+	}
+
 };
 }
 
